9a20796b635075b0d9d2feaebe62ddfee2f84d70
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2022-10-26 10:41:16 +0200

    4.4 Gestion de imagenes: casi terminado. Falta el controlador de imagenes y el reto

--- symfony-blog/src/Controller/AdminController.php
+++ symfony-blog/src/Controller/AdminController.php
@@ -6,6 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+use App\Entity\Category;
+
+use App\Form\CategoryFormType;
+
+use Doctrine\Persistence\ManagerRegistry ;
+
+use Symfony\Component\HttpFoundation\Request;
+
 class AdminController extends AbstractController
 {
 
@@ -17,12 +30,41 @@ class AdminController extends AbstractController
         return $this->render('admin/images.html.twig', []);
     }
 
-    public function adminDashboard(): Response
+    public function adminDashboard()
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
         // or add an optional message - seen by developers
         $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
     }
+
+        
+    /**
+     * @Route("/admin/categories", name="app_categories")
+     */
+    public function categories(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $repositorio = $doctrine->getRepository(Category::class);
+
+        $categories = $repositorio->findAll();
+
+        $category = new Category();
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();    
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
+        
+        return $this->render('admin/categories.html.twig', array(
+            'form' => $form->createView(),
+            'categories' => $categories   
+        ));
+
+    }
+
 
 }
