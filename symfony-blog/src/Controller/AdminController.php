7730c041b92f20bfe71015c0bb270c2413c7f694
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2022-10-26 10:12:34 +0200

    ZonaAdmin_4.3BloquearAccesoALaParteDeAdminstracion: terminado?????

--- symfony-blog/src/Controller/AdminController.php
+++ symfony-blog/src/Controller/AdminController.php
@@ -16,4 +16,13 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/images.html.twig', []);
     }
+
+    public function adminDashboard(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        // or add an optional message - seen by developers
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+    }
+
 }
