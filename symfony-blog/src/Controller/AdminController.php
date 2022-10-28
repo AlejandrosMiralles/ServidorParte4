04c78d5b22ec09119d55039f709f1587e9929aec
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2022-10-28 11:44:27 +0200

    ZonaDeAdministracion:4.4.4SubirImagenesAlServidor: Completado

--- symfony-blog/src/Controller/AdminController.php
+++ symfony-blog/src/Controller/AdminController.php
@@ -12,8 +12,10 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 use App\Entity\Category;
+use App\Entity\Image;
 
 use App\Form\CategoryFormType;
+use App\Form\ImageFormType ;
 
 use Doctrine\Persistence\ManagerRegistry ;
 
@@ -24,14 +26,84 @@ class AdminController extends AbstractController
 
     /**
      * @Route("/admin/images", name="app_images")
+     *
+    public function images(ManagerRegistry $doctrine, Request $request): Response{
+        $repositorio = $doctrine->getRepository(Image::class);
+
+        $images = $repositorio->findAll();
+
+        $image = new Image();
+        $form = $this->createForm(ImageFormType::class, $image);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $image = $form->getData();    
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($image);
+            $entityManager->flush();
+        }
+        
+        return $this->render('admin/images.html.twig', array(
+            'form' => $form->createView(),
+            'images' => $images 
+        ));
+
+    }
+    // */
+
+    /**
+     * @Route("/admin/images", name="app_images")
      */
-    public function images(): Response
-    {
-        return $this->render('admin/images.html.twig', []);
+    public function images(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger): Response{
+        
+        $repository = $doctrine->getRepository(Image::class);
+
+        $images = $repository->findAll();
+
+        $image = new Image();
+        $form = $this->createForm(ImageFormType::class, $image);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form->get('File')->getData();
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+        
+                // Move the file to the directory where images are stored
+                try {
+                    
+                    $file->move(
+                        $this->getParameter('images_directory'), $newFilename
+                    );
+                    $filesystem = new Filesystem();
+                    $filesystem->copy(
+                            $this->getParameter('images_directory') . '/'. $newFilename, 
+                            $this->getParameter('portfolio_directory') . '/'.  $newFilename, true);
+                   
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+        
+                // updates the 'file$filename' property to store the PDF file name
+                // instead of its contents
+                $image->setFile($newFilename);
+            }
+            $image = $form->getData();    
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($image);
+            $entityManager->flush();
+        }
+        return $this->render('admin/images.html.twig', array(
+            'form' => $form->createView(),
+            'images' => $images   
+        ));
     }
 
-    public function adminDashboard()
-    {
+    // */
+
+    public function adminDashboard(){
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
         // or add an optional message - seen by developers
@@ -42,8 +114,7 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin/categories", name="app_categories")
      */
-    public function categories(ManagerRegistry $doctrine, Request $request): Response
-    {
+    public function categories(ManagerRegistry $doctrine, Request $request): Response{
         $repositorio = $doctrine->getRepository(Category::class);
 
         $categories = $repositorio->findAll();
